3de8f84362e5ec7c6020c5e632b13adc581966e5
LittleCube <samuelcwhidden@gmail.com>
2020-08-09 22:38:15 -0400

    create namespace

--- source/UnsignedShort.h
+++ source/UnsignedShort.h
@@ -1,258 +1,261 @@
 #include "unsigned.h"
 
-UnsignedShort::UnsignedShort(char param)
+namespace Unsigned
 {
-	set(param);
-}
-
-UnsignedShort::UnsignedShort(short param)
-{
-	set(param);
-}
-
-UnsignedShort::UnsignedShort(int param)
-{
-	set(param);
-}
-
-UnsignedShort::UnsignedShort()
-{
-	set(0);
-}
-
-
-
-inline int UnsignedShort::unsign(short param)
-{
-	return param & 0xFFFF;
-}
-
-
-
-inline int UnsignedShort::get()
-{
-	return unsign(s);
-}
-
-inline int UnsignedShort::getBit(int position)
-{
-	return ((s & (0x1 << position)) >> position);
-}
-
-inline int UnsignedShort::subBits(int from, int to)
-{
-	int bits = 0;
-	
-	for (int i = from; i <= to; i++)
+	UnsignedShort::UnsignedShort(char param)
 	{
-		bits |= getBit(i) << (i - from);
+		set(param);
 	}
-	
-	return bits;
-}
 
-inline void UnsignedShort::setBit(int position, int value)
-{
-	value %= 2;
-	
-	if (value == 0)
+	UnsignedShort::UnsignedShort(short param)
 	{
-		s = (short) (s & ~((0x1 << position)));
+		set(param);
 	}
-	
-	else
+
+	UnsignedShort::UnsignedShort(int param)
 	{
-		s = (short) (s | (0x1 << position));
+		set(param);
 	}
-}
 
-inline void UnsignedShort::setBits(int from, int to, int *values)
-{
-	for (int i = 0; i <= to - from; i++)
+	UnsignedShort::UnsignedShort()
 	{
-		setBit(from + i, values[i]);
+		set(0);
 	}
-}
 
 
 
-inline void UnsignedShort::setBits(int from, int to, int directValue)
-{
-	for (int i = from; i <= to; i++)
+	inline int UnsignedShort::unsign(short param)
 	{
-		setBit(i, ((directValue & (0x1 << (i - from))) >> (i - from)));
+		return param & 0xFFFF;
 	}
-}
 
 
 
-inline void UnsignedShort::setByte(int position, int nbyte)
-{
-	nbyte = nbyte & 0xFF;
-	
-	int actualPosition = position * 8;
-	
-	s = (short) ((nbyte << actualPosition) | (s & ~(0xFF << actualPosition)));
-}
+	inline int UnsignedShort::get()
+	{
+		return unsign(s);
+	}
 
-inline char UnsignedShort::subByte(int position)
-{
-	int actualPosition = position * 8;
-	
-	return (char) ((s & (0xFF << (actualPosition))) >> actualPosition);
-}
+	inline int UnsignedShort::getBit(int position)
+	{
+		return ((s & (0x1 << position)) >> position);
+	}
 
-inline void UnsignedShort::craftShort(char byte1, char byte0)
-{
-	setByte(1, byte1);
-	setByte(0, byte0);
-}
+	inline int UnsignedShort::subBits(int from, int to)
+	{
+		int bits = 0;
+		
+		for (int i = from; i <= to; i++)
+		{
+			bits |= getBit(i) << (i - from);
+		}
+		
+		return bits;
+	}
 
+	inline void UnsignedShort::setBit(int position, int value)
+	{
+		value %= 2;
+		
+		if (value == 0)
+		{
+			s = (short) (s & ~((0x1 << position)));
+		}
+		
+		else
+		{
+			s = (short) (s | (0x1 << position));
+		}
+	}
 
-
-inline void UnsignedShort::left(int offset)
-{
-	s <<= offset;
-}
-
-inline void UnsignedShort::right(int offset)
-{
-	s >>= offset;
-}
-
-inline void UnsignedShort::comp()
-{
-	s = (short) ~s;
-}
-
-
-
-// -------- UnsignedShort parameters --------
-inline void UnsignedShort::set(UnsignedShort param)
-{
-	s = param.s;
-}
-
-inline void UnsignedShort::add(UnsignedShort param)
-{
-	s += param.s;
-}
-
-inline void UnsignedShort::sub(UnsignedShort param)
-{
-	add(-param.s);
-}
-
-inline void UnsignedShort::_or(UnsignedShort param)
-{
-	s |= param.s;
-}
-
-inline void UnsignedShort::_and(UnsignedShort param)
-{
-	s &= param.s;
-}
-
-inline void UnsignedShort::_xor(UnsignedShort param)
-{
-	s ^= param.s;
-}
+	inline void UnsignedShort::setBits(int from, int to, int *values)
+	{
+		for (int i = 0; i <= to - from; i++)
+		{
+			setBit(from + i, values[i]);
+		}
+	}
 
 
 
-// ------- char parameters --------
-inline void UnsignedShort::set(char param)
-{
-	s = (short) param;
-}
-
-inline void UnsignedShort::add(char param)
-{
-	s += param;
-}
-
-inline void UnsignedShort::sub(char param)
-{
-	add(-param);
-}
-
-inline void UnsignedShort::_or(char param)
-{
-	s |= param;
-}
-
-inline void UnsignedShort::_and(char param)
-{
-	s &= param;
-}
-
-inline void UnsignedShort::_xor(char param)
-{
-	s ^= param;
-}
+	inline void UnsignedShort::setBits(int from, int to, int directValue)
+	{
+		for (int i = from; i <= to; i++)
+		{
+			setBit(i, ((directValue & (0x1 << (i - from))) >> (i - from)));
+		}
+	}
 
 
 
-// ------ short parameters --------
-inline void UnsignedShort::set(short param)
-{
-	s = param;
-}
+	inline void UnsignedShort::setByte(int position, int nbyte)
+	{
+		nbyte = nbyte & 0xFF;
+		
+		int actualPosition = position * 8;
+		
+		s = (short) ((nbyte << actualPosition) | (s & ~(0xFF << actualPosition)));
+	}
 
-inline void UnsignedShort::add(short param)
-{
-	s += param;
-}
+	inline char UnsignedShort::subByte(int position)
+	{
+		int actualPosition = position * 8;
+		
+		return (char) ((s & (0xFF << (actualPosition))) >> actualPosition);
+	}
 
-inline void UnsignedShort::sub(short param)
-{
-	add(-param);
-}
-
-inline void UnsignedShort::_or(short param)
-{
-	s |= param;
-}
-
-inline void UnsignedShort::_and(short param)
-{
-	s &= param;
-}
-
-inline void UnsignedShort::_xor(short param)
-{
-	s ^= param;
-}
+	inline void UnsignedShort::craftShort(char byte1, char byte0)
+	{
+		setByte(1, byte1);
+		setByte(0, byte0);
+	}
 
 
 
-// -------- int parameters --------
-inline void UnsignedShort::set(int param)
-{
-	s = (short) param;
-}
+	inline void UnsignedShort::left(int offset)
+	{
+		s <<= offset;
+	}
 
-inline void UnsignedShort::add(int param)
-{
-	s += param;
-}
+	inline void UnsignedShort::right(int offset)
+	{
+		s >>= offset;
+	}
 
-inline void UnsignedShort::sub(int param)
-{
-	add(-param);
-}
+	inline void UnsignedShort::comp()
+	{
+		s = (short) ~s;
+	}
 
-inline void UnsignedShort::_or(int param)
-{
-	s |= param;
-}
 
-inline void UnsignedShort::_and(int param)
-{
-	s &= param;
-}
 
-inline void UnsignedShort::_xor(int param)
-{
-	s ^= param;
+	// -------- UnsignedShort parameters --------
+	inline void UnsignedShort::set(UnsignedShort param)
+	{
+		s = param.s;
+	}
+
+	inline void UnsignedShort::add(UnsignedShort param)
+	{
+		s += param.s;
+	}
+
+	inline void UnsignedShort::sub(UnsignedShort param)
+	{
+		add(-param.s);
+	}
+
+	inline void UnsignedShort::_or(UnsignedShort param)
+	{
+		s |= param.s;
+	}
+
+	inline void UnsignedShort::_and(UnsignedShort param)
+	{
+		s &= param.s;
+	}
+
+	inline void UnsignedShort::_xor(UnsignedShort param)
+	{
+		s ^= param.s;
+	}
+
+
+
+	// ------- char parameters --------
+	inline void UnsignedShort::set(char param)
+	{
+		s = (short) param;
+	}
+
+	inline void UnsignedShort::add(char param)
+	{
+		s += param;
+	}
+
+	inline void UnsignedShort::sub(char param)
+	{
+		add(-param);
+	}
+
+	inline void UnsignedShort::_or(char param)
+	{
+		s |= param;
+	}
+
+	inline void UnsignedShort::_and(char param)
+	{
+		s &= param;
+	}
+
+	inline void UnsignedShort::_xor(char param)
+	{
+		s ^= param;
+	}
+
+
+
+	// ------ short parameters --------
+	inline void UnsignedShort::set(short param)
+	{
+		s = param;
+	}
+
+	inline void UnsignedShort::add(short param)
+	{
+		s += param;
+	}
+
+	inline void UnsignedShort::sub(short param)
+	{
+		add(-param);
+	}
+
+	inline void UnsignedShort::_or(short param)
+	{
+		s |= param;
+	}
+
+	inline void UnsignedShort::_and(short param)
+	{
+		s &= param;
+	}
+
+	inline void UnsignedShort::_xor(short param)
+	{
+		s ^= param;
+	}
+
+
+
+	// -------- int parameters --------
+	inline void UnsignedShort::set(int param)
+	{
+		s = (short) param;
+	}
+
+	inline void UnsignedShort::add(int param)
+	{
+		s += param;
+	}
+
+	inline void UnsignedShort::sub(int param)
+	{
+		add(-param);
+	}
+
+	inline void UnsignedShort::_or(int param)
+	{
+		s |= param;
+	}
+
+	inline void UnsignedShort::_and(int param)
+	{
+		s &= param;
+	}
+
+	inline void UnsignedShort::_xor(int param)
+	{
+		s ^= param;
+	}
 }
--- source/unsigned.h
+++ source/unsigned.h
@@ -1,122 +1,125 @@
-class UnsignedByte
+namespace Unsigned
 {
-	public:
-		char b;
-		
-		UnsignedByte(char param);
-		UnsignedByte(short param);
-		UnsignedByte(int param);
-		UnsignedByte();
-		
-		int unsign(char param);
-		
-		int get();
-		int getBit(int position);
-		
-		int subBits(int from, int to);
-		
-		void setBit(int position, int value);
-		
-		void setBits(int from, int to, int* values);
-		void setBits(int from, int to, int directValue);
-		
-		void left(int offset);
-		void right(int offset);
-		
-		void comp();
-		
-		void set(UnsignedByte param);
-		void add(UnsignedByte param);
-		void sub(UnsignedByte param);
-		void _or(UnsignedByte param);
-		void _and(UnsignedByte param);
-		void _xor(UnsignedByte param);
-		
-		void set(char param);
-		void add(char param);
-		void sub(char param);
-		void _or(char param);
-		void _and(char param);
-		void _xor(char param);
-		
-		void set(short param);
-		void add(short param);
-		void sub(short param);
-		void _or(short param);
-		void _and(short param);
-		void _xor(short param);
-		
-		void set(int param);
-		void add(int param);
-		void sub(int param);
-		void _or(int param);
-		void _and(int param);
-		void _xor(int param);
-};
-
-class UnsignedShort
-{
-	public:
-		short s;
-		
-		UnsignedShort(char param);
-		UnsignedShort(short param);
-		UnsignedShort(int param);
-		UnsignedShort();
-		
-		int unsign(short param);
-		
-		int get();
-		int getBit(int position);
-		
-		int subBits(int from, int to);
-		
-		void setBit(int position, int value);
-		
-		void setBits(int from, int to, int* values);
-		void setBits(int from, int to, int directValue);
-		
-		void setByte(int position, int nbyte);
-		char subByte(int position);
-		void craftShort(char nbyte1, char nbyte0);
-		
-		void left(int offset);
-		void right(int offset);
-		
-		void comp();
-		
-		void set(UnsignedShort param);
-		void add(UnsignedShort param);
-		void sub(UnsignedShort param);
-		void _or(UnsignedShort param);
-		void _and(UnsignedShort param);
-		void _xor(UnsignedShort param);
-		
-		void set(UnsignedByte param);
-		void add(UnsignedByte param);
-		void sub(UnsignedByte param);
-		void _or(UnsignedByte param);
-		void _and(UnsignedByte param);
-		void _xor(UnsignedByte param);
-		
-		void set(char param);
-		void add(char param);
-		void sub(char param);
-		void _or(char param);
-		void _and(char param);
-		void _xor(char param);
-		
-		void set(short param);
-		void add(short param);
-		void sub(short param);
-		void _or(short param);
-		void _and(short param);
-		void _xor(short param);
-		
-		void set(int param);
-		void add(int param);
-		void sub(int param);
-		void _or(int param);
-		void _and(int param);
-		void _xor(int param);
-};
+	class UnsignedByte
+	{
+		public:
+			char b;
+			
+			UnsignedByte(char param);
+			UnsignedByte(short param);
+			UnsignedByte(int param);
+			UnsignedByte();
+			
+			int unsign(char param);
+			
+			int get();
+			int getBit(int position);
+			
+			int subBits(int from, int to);
+			
+			void setBit(int position, int value);
+			
+			void setBits(int from, int to, int* values);
+			void setBits(int from, int to, int directValue);
+			
+			void left(int offset);
+			void right(int offset);
+			
+			void comp();
+			
+			void set(UnsignedByte param);
+			void add(UnsignedByte param);
+			void sub(UnsignedByte param);
+			void _or(UnsignedByte param);
+			void _and(UnsignedByte param);
+			void _xor(UnsignedByte param);
+			
+			void set(char param);
+			void add(char param);
+			void sub(char param);
+			void _or(char param);
+			void _and(char param);
+			void _xor(char param);
+			
+			void set(short param);
+			void add(short param);
+			void sub(short param);
+			void _or(short param);
+			void _and(short param);
+			void _xor(short param);
+			
+			void set(int param);
+			void add(int param);
+			void sub(int param);
+			void _or(int param);
+			void _and(int param);
+			void _xor(int param);
+	};
+	
+	class UnsignedShort
+	{
+		public:
+			short s;
+			
+			UnsignedShort(char param);
+			UnsignedShort(short param);
+			UnsignedShort(int param);
+			UnsignedShort();
+			
+			int unsign(short param);
+			
+			int get();
+			int getBit(int position);
+			
+			int subBits(int from, int to);
+			
+			void setBit(int position, int value);
+			
+			void setBits(int from, int to, int* values);
+			void setBits(int from, int to, int directValue);
+			
+			void setByte(int position, int nbyte);
+			char subByte(int position);
+			void craftShort(char nbyte1, char nbyte0);
+			
+			void left(int offset);
+			void right(int offset);
+			
+			void comp();
+			
+			void set(UnsignedShort param);
+			void add(UnsignedShort param);
+			void sub(UnsignedShort param);
+			void _or(UnsignedShort param);
+			void _and(UnsignedShort param);
+			void _xor(UnsignedShort param);
+			
+			void set(UnsignedByte param);
+			void add(UnsignedByte param);
+			void sub(UnsignedByte param);
+			void _or(UnsignedByte param);
+			void _and(UnsignedByte param);
+			void _xor(UnsignedByte param);
+			
+			void set(char param);
+			void add(char param);
+			void sub(char param);
+			void _or(char param);
+			void _and(char param);
+			void _xor(char param);
+			
+			void set(short param);
+			void add(short param);
+			void sub(short param);
+			void _or(short param);
+			void _and(short param);
+			void _xor(short param);
+			
+			void set(int param);
+			void add(int param);
+			void sub(int param);
+			void _or(int param);
+			void _and(int param);
+			void _xor(int param);
+	};
+}
